336697adfb7e4e2973d79ae2d5e25a96fc9e2c2f
Kalid Yasin <kalidyasin29@gmail.com>
2025-04-09 10:09:53 +0300

    fix: date format

--- src/components/video_card.rs
+++ src/components/video_card.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use yew::prelude::*;
 
 use crate::api::youtube::youtube_api::PlaylistItem;
@@ -10,23 +11,32 @@ pub struct VideoCardProps {
 
 #[function_component(VideoCard)]
 pub fn video_card(VideoCardProps { video, index }: &VideoCardProps) -> Html {
+    let formatted_date = format_published_date(&video.snippet.published_at);
     html! {
-        <div key={*index} class="w-full overflow-hidden shadow-xl bg-gray-300 hover:bg-gray-400 dark:bg-gray-700/40 dark:hover:bg-gray-700/60 y-6 rounded-xl border border-gray-700">
+        <div key={*index} class="w-full overflow-hidden shadow-xl bg-gray-300 hover:bg-gray-400 dark:bg-gray-700/40 dark:hover:bg-gray-700/60 y-6 rounded-xl border border-gray-700 duration-300 group">
             <a href={format!("https://youtu.be/{}", video.content_details.video_id)} target="_blank" class="no-underline">
                 <img
-                    class="w-full m-0 mb-1 rounded-t-xl hover:scale-105 transition-transform ease-in-out duration-500"
+                    class="w-full m-0 mb-1 rounded-t-xl group-hover:scale-105 transition-transform ease-in-out duration-500"
                     src={video.snippet.thumbnails.medium.url.clone()}
                     alt={video.snippet.title.clone()}
                 />
                 <section class="p-5">
                     <h5 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white mb-3">{ video.snippet.title.clone() }</h5>
                     <p class="text-gray-700 dark:text-gray-400 min-h-[100px]">{ video.snippet.title.clone() }</p>
-                    <span class="text-gray-700 dark:text-gray-400">{ video.snippet.published_at.clone() }</span>
-                    <span class="tag inline-flex bg-green-500 items-center rounded-md mx-1 text-sm font-medium px-1 py-1">
+                    // <span class="text-gray-700 dark:text-gray-400">{ formatted_date }</span><br/>
+                    /* <span class="tag inline-flex bg-green-500 items-center rounded-md mx-1 text-sm font-medium px-1 py-1">
                         {"#Rust"}
+                    </span>*/
+                    <span class="tag text-white inline-flex bg-green-500 dark:bg-green-500 items-center rounded-md mx-1 text-sm font-extrabold px-1 py-1">
+                        { formatted_date }
                     </span>
                 </section>
             </a>
         </div>
     }
+}
+
+fn format_published_date(date_string: &str) -> String {
+    let datetime = DateTime::parse_from_rfc3339(date_string).unwrap_or_else(|_| Utc::now().into()); // Fallback to current time if parsing fails
+    datetime.format("%d %b %Y").to_string()
 }
